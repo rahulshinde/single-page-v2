99b14b63138b218df16a6506be2e33ff9a010132
Rahul Subhash Shinde <rshinde@mica.edu>
2016-06-10 21:18:51 -0600

    reset

--- js/hov.js
+++ js/hov.js
@@ -7,66 +7,66 @@ $(document).ready( function() {
 	// hov1
 	$( '#hov1' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/2.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/2.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 
     // hov2
 	$( '#hov2' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/3.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/3.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 
     // hov3
 	$( '#hov3' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/4.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/4.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 
     // hov4
 	$( '#hov4' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/5.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/5.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 
     // hov5
 	$( '#hov5' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/6.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/6.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 
     // hov6
 	$( '#hov6' )
 		.mouseover(function() {
-            $('#thumb').css('background-image', 'url(img/7.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/7.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         })
         .mouseout(function() {
-        	$('#thumb').css('background-image', 'url(img/1.jpg)');
+        	$('#thumb').css('background-image', 'url(../img/1.jpg)');
         	$('#thumb-desc').html('Above images generated using Image-Outliner, a Python script written in early 2016');
         });
 });
