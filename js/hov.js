83770f1d770c6ccf69ff6fe0515199be8abcdf16
Rahul Subhash Shinde <rshinde@mica.edu>
2016-06-10 21:34:46 -0600

    forgot js

--- js/hov.js
+++ js/hov.js
@@ -1,48 +1,48 @@
 // HOV.JS
 // no jay-z here, sorry :(
-// changes img/description on link hover
+// changes img description on link hover
 
 $(document).ready( function() {
 
 	// hov1
 	$( '#hov1' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/2.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/2.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 
     // hov2
 	$( '#hov2' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/3.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/3.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 
     // hov3
 	$( '#hov3' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/4.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/4.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 
     // hov4
 	$( '#hov4' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/5.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/5.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 
     // hov5
 	$( '#hov5' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/6.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/6.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 
     // hov6
 	$( '#hov6' )
 		.hover(function() {
-            $('#thumb').css('background-image', 'url(../img/7.jpg)');  
+            $('#thumb').css('background-image', 'url(../img/landing/7.jpg)');  
             $('#thumb-desc').html('Branding and Visual Systems');
         });
 });
