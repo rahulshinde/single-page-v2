ce2a0cb9884ddb7d47755a88a7a75d2718ab055c
Rahul Subhash Shinde <rshinde@mica.edu>
2016-06-15 23:06:44 -0600

    final touches for now will minify later

--- js/hov.js
+++ js/hov.js
@@ -8,71 +8,59 @@ $(document).ready( function() {
 	$( '#hov1' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/2.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 
     // hov2
 	$( '#hov2' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/3.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 
     // hov3
 	$( '#hov3' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/4.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 
     // hov4
 	$( '#hov4' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/5.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 
     // hov5
 	$( '#hov5' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/6.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 
     // hov6
 	$( '#hov6' )
 		.mouseover(function() { 
             $('#thumb').attr("src", 'img/landing/7.jpg');
-            $('#thumb-desc').html('Branding and Visual Systems');
         })
 
         .mouseout(function() {
             $('#thumb').attr("src", 'img/landing/1.jpg');
-            $('#thumb-desc').html('Above images generated using a Python script written in early 2016');  
         });
 });
